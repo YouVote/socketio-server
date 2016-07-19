c9a0cabafb2a84a7fe33be61f3373009d62723b1
Gabriel Wu <gabrielwu84@gmail.com>
2016-07-20 02:16:30 +0800

    completed server code.
    updated install instructions
    
    Added info for openshift deployment
    
    - Changed version to 1.0.0
    - Added main info
    - Added "--use_strict" to start

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ Nodejs server for Avalon eVoter.
 
 Installation
 ------------
+Run the following commands:
 ```
+git init
+git remote add origin https://github.com/gabrielwu84/avalon-server
+git pull
 npm install
 npm start
 ```
--- server.js
+++ server.js
@@ -1,31 +1,90 @@
 console.log('socket listening on port 23784');
 var io = require('socket.io').listen(23784);
 
-var gameRoom={
-	// new game method (generate gameId check nonexistant)
-	// store host socket
-	// playerconnect method
-	// store player sockets
-	// hostdisconnect method
-	// playerdisconnect method
-	// communication between host and players
+function game(hostSocket){
+	var players={};
+	var host=hostSocket;
+	this.playerJoin=function(socket){
+		host.emit('playerJoin',socket.id);
+		players[socket.id]=socket;
+		console.log(Object.keys(players));
+	}
+	this.playerQuit=function(playerSocketId){
+		host.emit('playerQuit',playerSocketId);
+		delete players[playerSocketId];
+		console.log(Object.keys(players));
+	}
+	this.hostQuit=function(){
+		for(var idx in players){
+			players[idx].emit('shutdown','host closed game');
+			players[idx].disconnect();
+		} 
+	}
+	this.hostToPlayer=function(playerSocketId,msg){
+		players[playerSocketId].emit('relay',msg);
+	}
+	this.playerToHost=function(playerSocketId,msg){
+		host.emit('relay',{'socketId':playerSocketId,'msg':msg});
+	}
 }
+
+var gameEngine={
+	gameIds:{},
+	newGame:function(hostSocket){
+		var generateId=require('./genId').genStr;
+		gameId=generateId(6);
+		while(gameId in gameEngine.gameIds){
+			gameId=generateId(6);
+		}
+		var gameObj=new game(hostSocket);
+		gameEngine.gameIds[gameId]=gameObj;
+		return {'gameId':gameId,'gameObj':gameObj};
+	},
+	getGame:function(gameId){
+		return gameEngine.gameIds[gameId];
+	},
+	delGame:function(gameId){
+		delete gameEngine.gameIds[gameId];
+	}
+};
 
 io.on('connect',function(socket){
 	console.log('new connection');
-	socket.gamedata={};
+	socket.gameData={};
 	socket.emit('connectType?');
-	socket.on('connectType',function(data){
-		
+	socket.on('connectType=',function(data){
 		console.log('Connection Type: '+data.type);
-		// switch type
-		socket.gamedata.usertype=data.type;
-		switch(socket.gamedata.usertype){
+		socket.gameData.usertype=data.type;
+		switch(socket.gameData.usertype){
 			case 'host':
-				// get gameid and set up room
-				socket.emit("newGameId=","1a2b3c");
+				gameParam=gameEngine.newGame(socket);
+				socket.gameData.gameId=gameParam.gameId;
+				socket.gameData.gameObj=gameParam.gameObj;
+				socket.emit("newGameId=",socket.gameData.gameId);
 				break;
 			case 'play':
+				socket.gameData.gameId=data.gameId;
+				socket.gameData.gameObj=gameEngine.getGame(socket.gameData.gameId);
+				if(typeof(socket.gameData.gameObj)=='object'){
+					socket.gameData.gameObj.playerJoin(socket);
+					socket.emit("gameStatus");
+				} else {
+					socket.emit('serverShutDown','game not found '+socket.gameData.gameId);
+					socket.disconnect();
+				}
+				break;
+			default:
+				socket.emit('serverShutDown','Unrecognized usertype '+socket.gamedata.usertype);
+				socket.disconnect();
+		}
+	});
+	socket.on('relay',function(data){
+		switch(socket.gameData.usertype){
+			case 'host':
+				socket.gameData.gameObj.hostToPlayer(data.socketId,data.msg);
+				break;
+			case 'play':
+				socket.gameData.gameObj.playerToHost(socket.id,data.msg);
 				break;
 			default:
 				socket.emit('serverShutDown','Unrecognized usertype '+socket.gamedata.usertype);
@@ -34,8 +93,19 @@ io.on('connect',function(socket){
 
 	});
 	socket.on('disconnect',function(){
-		// gameroom disconnect
-		console.log("Disconnect: "+JSON.stringify(socket.gamedata));
+		switch(socket.gameData.usertype){
+			case 'host':
+				socket.gameData.gameObj.hostQuit();
+				gameEngine.deleteGame(socket.gameData.gameId);
+				break;
+			case 'play':
+				socket.gameData.gameObj.playerQuit(socket.id);
+				break;
+			default:
+				socket.emit('serverShutDown','Unrecognized usertype '+socket.gamedata.usertype);
+				socket.disconnect();
+		}
+		console.log("Disconnect: "+JSON.stringify(socket.id));
 	});
 
 	setTimeout(function(){
